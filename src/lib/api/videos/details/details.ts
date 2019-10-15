7d3139bd2d9dc8ba8e54d99a166337b06b3665fe
Iuri Neumann <iuri.neumann@t2mlab.com>
2019-10-15 09:55:09 -0300

    v1.1.18

--- src/lib/api/videos/details/details.ts
+++ src/lib/api/videos/details/details.ts
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { load } from 'cheerio';
-import { launch, Browser } from 'puppeteer';
+import { launch, Browser, connect } from 'puppeteer';
 import VideoDetails from '../response/videoDetails';
 
 const details = async ({ url }: { url: string }): Promise<VideoDetails> => {
   let browser: Browser;
   try {
-    browser = await launch();
+    if (process.env.PUPPETEER_WS_BROWSER) {
+      browser = await connect({ browserWSEndpoint: process.env.PUPPETEER_WS_BROWSER });
+    } else {
+      browser = await launch({ headless: true, ignoreHTTPSErrors: true, handleSIGHUP: true });
+    }
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
     const html = await page.content();
